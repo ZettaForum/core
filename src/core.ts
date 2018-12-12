100b7512d59db08d21f01dc26c6b26c437697799
Xmader <xmader@outlook.com>
2018-12-12 11:18:59 +0800

    更改定义默认值的方式

--- src/core.ts
+++ src/core.ts
@@ -7,22 +7,38 @@
 import { Translator, addTranslations } from "./translate"
 import { Theme, getThemeFromName, defineThemeName } from "./theme"  // eslint-disable-line
 
+interface NavItem {
+    name: string,
+    href: string,
+}
+
+interface TopNavItem extends NavItem {
+    icon?: string
+}
+
 export interface Init {
     name: string,
     el?: string,
     theme?: string | Theme,
     language?: string,
     logo?: string,
-    favicon?: string,
-    nameLink?: string,
+    logoLink?: string,
+    topNav: TopNavItem[],  // Max Length: 5
     routerMode?: "hash" | "history"
 }
 
 export default class ZettaForum {
     constructor(init: Init) {
-        init.theme = init.theme || "wecenter"
-        init.language = init.language || "zh_CN"
-        init.logo = init.logo || "logo.png"
+
+        // Define the default values
+        init = Object.assign({
+            el: "#app",
+            theme: "wecenter",
+            language: "zh_CN",
+            logo: "logo.png",
+            logoLink: "/",
+            routerMode: "hash",
+        }, init)
 
         let theme: Theme = null
         if (typeof init.theme == "string") {
